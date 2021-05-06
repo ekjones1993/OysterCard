6d3e9ee8efba62b4bd5f49379083b73e7e5f4537
ekjones1993 <emilykjones1993@gmail.com>
2021-05-06 11:49:28 +0100

    refactored spec

--- spec/OysterCard_spec.rb
+++ spec/OysterCard_spec.rb
@@ -1,52 +1,50 @@
 require 'OysterCard'
 
 describe OysterCard do
-  let(:station){ double :station }
+  let(:entrystation) { double :station }
+  let(:exitstation) { double :station }
+
   describe '#journey' do
     it "is not initially in journey" do
       expect(subject.in_journey?) == false
     end
+  end
 
-    it "can touch in" do
+  describe '#touchin' do
+    before do
       subject.topup(10)
-      subject.touchin(station)
+      subject.touchin(entrystation)
+    end
+    it "can touch in" do
       expect(subject.in_journey?) == true
     end
 
-    it "won't touch in if below MIN_BALANCE" do
-      expect { subject.touchin(station) }.to raise_error "Balance too low"
-    end
-
     it "stores the entry station" do
+      expect(subject.entrystation).to eq entrystation
+    end
+  end
+
+  describe '#touchout' do
+    before do
       subject.topup(10)
-      subject.touchin(station)
-      expect(subject.entrystation).to eq station
+      subject.touchin(entrystation)
+      subject.touchout(exitstation)
     end
 
     it "can touchout" do
-      subject.topup(10)
-      subject.touchin(station)
-      subject.touchout(station)
       expect(subject.in_journey?) == false
     end
 
     it "deducts minimum charge on touch out" do
-      subject.topup(10)
-      subject.touchin(station)
-      expect { subject.touchout(station) }.to change { subject.balance }.by(-OysterCard::MIN_CHARGE)
+      expect { subject.touchout(exitstation) }.to change { subject.balance }.by(-OysterCard::MIN_CHARGE)
     end
 
     it "forgets entry station on touchout" do
-      subject.topup(10)
-      subject.touchin(station)
-      subject.touchout(station)
       expect(subject.entrystation).to eq nil
     end
 
     it "stores the exit station" do
-      subject.topup(10)
-      subject.touchout("euston")
-      expect(subject.exitstation).to eq "euston"
+      expect(subject.exitstation).to eq exitstation
     end
   end
 
@@ -54,13 +52,16 @@ describe OysterCard do
     it "has a balance of 0" do
       expect(subject.balance).to eq(0)
     end
+
+    it "won't touch in if below MIN_BALANCE" do
+      expect { subject.touchin(entrystation) }.to raise_error "Balance too low"
+    end
   end
 
   describe '#topup' do
     it { is_expected.to respond_to(:topup).with(1).argument }
 
     it "can add to the balance" do
-
       expect{ subject.topup(1) }.to change{ subject.balance }.by 1
     end
 
